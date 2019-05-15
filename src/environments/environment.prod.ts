f91fc5853aa4d6826665608b0e2d8b0b5a17c1c6
unknown <lordguerrero94@gmail.com>
2019-05-16 00:32:34 +0200

    Added AWS serverless publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_secret: '>P8h(}idkbUu(z;=3@}x',
+  api_url: 'http://35.180.225.164:3000/api/v1',
+  localhost_api_url: 'http://localhost:3000/api/v1'
 };
