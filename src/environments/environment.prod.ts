2f1f3e1a89799997d70643a7562e36ad9c8866f7
Pablo Guerrero <p.guerrero.gonzalez@accenture.com>
2019-06-10 09:00:40 +0200

    updated gitignore and environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api_secret: '>P8h(}idkbUu(z;=3@}x',
-  api_url: 'http://35.180.225.164:3000/api/v1',
-  socket_url: 'http://35.180.225.164:4444'
+  api_url: 'http://localhost:3000/api/v1',
+  socket_url: 'http://localhost:4444'
 };
